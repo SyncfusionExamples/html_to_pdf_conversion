953d0ec0544071ac50fe76a8cde467af24e98674
Praveen <praveenh@syncfusion.com>
2021-07-27 10:48:05 +0530

    Added the new samples

---
+++ Automatic_bookmarks/Automatic_bookmarks/Program.cs
@@ -0,0 +1,39 @@
+﻿using Syncfusion.Drawing;
+using Syncfusion.HtmlConverter;
+using Syncfusion.Pdf;
+using Syncfusion.Pdf.Graphics;
+using Syncfusion.Pdf.HtmlToPdf;
+using System;
+using System.IO;
+
+namespace Automatic_bookmarks
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Initialize HTML to PDF converter with Blink rendering engine
+            HtmlToPdfConverter htmlConverter = new HtmlToPdfConverter(HtmlRenderingEngine.Blink);
+
+            BlinkConverterSettings blinkConverterSettings = new BlinkConverterSettings();
+
+            //Set the BlinkBinaries folder path
+            blinkConverterSettings.BlinkPath = @"../../../../../BlinkBinariesWindows/";
+
+            //Enable automatic bookmark creation
+            blinkConverterSettings.EnableBookmarks = true;
+
+            //Assign Blink converter settings to HTML converter
+            htmlConverter.ConverterSettings = blinkConverterSettings;
+
+            //Convert HTML File to PDF
+            PdfDocument document = htmlConverter.Convert("https://help.syncfusion.com/file-formats/pdf/convert-html-to-pdf/blink");
+
+            FileStream fileStream = new FileStream("bookmark_creation.pdf", FileMode.OpenOrCreate, FileAccess.ReadWrite);
+
+            //Save and close the PDF document 
+            document.Save(fileStream);
+            document.Close(true);
+        }
+    }
+}
---
+++ Automatic_toc_creation/Automatic_toc_creation/Program.cs
@@ -0,0 +1,54 @@
+﻿using Syncfusion.Drawing;
+using Syncfusion.HtmlConverter;
+using Syncfusion.Pdf;
+using Syncfusion.Pdf.Graphics;
+using Syncfusion.Pdf.HtmlToPdf;
+using System;
+using System.IO;
+
+namespace Automatic_toc_creation
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Initialize HTML to PDF converter with Blink rendering engine
+            HtmlToPdfConverter htmlConverter = new HtmlToPdfConverter(HtmlRenderingEngine.Blink);
+
+            BlinkConverterSettings blinkConverterSettings = new BlinkConverterSettings();
+
+            //Set the BlinkBinaries folder path
+            blinkConverterSettings.BlinkPath = @"../../../../../BlinkBinariesWindows/";
+
+            //Enable automati TOC creation
+            blinkConverterSettings.EnableToc = true;
+
+            //Set the style for level 1(H1) items in table of contents
+
+            HtmlToPdfTocStyle tocstyleH1 = new HtmlToPdfTocStyle();
+
+            tocstyleH1.Font = new PdfStandardFont(PdfFontFamily.TimesRoman, 10, PdfFontStyle.Regular);
+
+            tocstyleH1.BackgroundColor = new PdfSolidBrush(new PdfColor(Color.FromArgb(68, 114, 196)));
+
+            tocstyleH1.ForeColor = PdfBrushes.White;
+
+            tocstyleH1.Padding = new PdfPaddings(5, 5, 3, 3);
+
+            //Apply this style to level 1 (H1) headings of the TOC
+            blinkConverterSettings.Toc.SetItemStyle(1, tocstyleH1);
+
+            //Assign Blink converter settings to HTML converter
+            htmlConverter.ConverterSettings = blinkConverterSettings;
+
+            //Convert HTML File to PDF
+            PdfDocument document = htmlConverter.Convert("https://help.syncfusion.com/file-formats/pdf/convert-html-to-pdf/blink");
+
+            FileStream fileStream = new FileStream("toc_creation.pdf", FileMode.OpenOrCreate, FileAccess.ReadWrite);
+
+            //Save and close the PDF document 
+            document.Save(fileStream);
+            document.Close(true);
+        }
+    }
+}
