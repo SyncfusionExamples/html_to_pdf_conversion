5df8b1c7c8e65c60821ecaaa324b36d04b3e69c3
Karmegam <103259299+karmegams02@users.noreply.github.com>
2024-08-06 14:49:18 +0530

    Delete Performance_Testing/IronPDF_HTMLtoPDF directory

--- Performance_Testing/IronPDF_HTMLtoPDF/Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace IronPDF_HTMLtoPDF.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
